2f98c28104823b228dc72a3887da3e116f2bcda3
rababni-essaly <golam.rabbani@essaly.com>
2023-08-31 11:47:51 +0300

    asdf

--- index.js
+++ index.js
@@ -4,11 +4,6 @@ app.get('/', (req, res)=>{
     res.send('App is running');
 })
 
-app.get('/sec', (req, res)=>{
-    res.send('Second api');
-})
-
-
 app.listen(process.env.PROD_PORT, ()=>{
     console.log('app port+ ', process.env.PROD_PORT);
 })
