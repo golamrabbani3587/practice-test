f0ff6a93864c569408d1655ca187e690defa30fe
rababni-essaly <golam.rabbani@essaly.com>
2023-09-03 14:33:27 +0300

    test-1

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ require('dotenv').config()
 app.get('/', (req, res)=>{
     res.send('App is running');
 })
+
+app.get('/test', (req, res)=>{
+    res.send('test one success');
+})
+
+
 app.listen(process.env.PROD_PORT, ()=>{
     console.log('app port +', process.env.PROD_PORT);
 })
