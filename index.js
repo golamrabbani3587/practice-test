c359fffc848a084cba23f2e10bb004fa6e287be3
rababni-essaly <golam.rabbani@essaly.com>
2023-09-03 14:22:28 +0300

    dfdf

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ require('dotenv').config()
 app.get('/', (req, res)=>{
     res.send('App is running');
 })
-
 app.listen(process.env.PROD_PORT, ()=>{
     console.log('app port +', process.env.PROD_PORT);
 })
