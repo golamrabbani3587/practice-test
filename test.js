d872bcd8e8207646cf87e04a11d525a479bb1886
rababni-essaly <golam.rabbani@essaly.com>
2023-09-03 13:50:06 +0300

    asdfg

--- test.js
+++ test.js
@@ -2,7 +2,6 @@ var expect  = require('chai').expect;
 var request = require('request');
 require('dotenv').config()
 
-
 it('Main page content', function(done) {
     request('http://localhost:'+process.env.PROD_PORT, function(error, response, body) {
         expect(body).to.equal('App is running');
