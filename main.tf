8fe008bac76ef892f7dfbee4d3f2a652ff3e183a
rababni-essaly <golam.rabbani@essaly.com>
2023-09-04 16:04:26 +0300

    ffgfg

--- main.tf
+++ main.tf
@@ -1,8 +1,21 @@
 # Define the DigitalOcean provider
 provider "digitalocean" {
   token = "dop_v1_9ba2628ece364a2cf0ce1c413850b1db1cc6b17659797a1e710954e13f450073"
-   version = "1.5.6"
+  version = "1.5.6"
 }
+
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      token = "dop_v1_9ba2628ece364a2cf0ce1c413850b1db1cc6b17659797a1e710954e13f450073"
+      version = "1.5.6"
+    }
+  }
+}
+
+
+
 resource "digitalocean_droplet" "blue_server" {
   name    = "blue-server"
   region  = "nyc1" # Replace with your desired region
