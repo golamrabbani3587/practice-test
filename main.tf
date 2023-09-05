5c72382892f234a4dbc1a5bfb4760c2b16ac8613
rababni-essaly <golam.rabbani@essaly.com>
2023-09-05 10:44:54 +0300

    asdf

--- main.tf
+++ main.tf
@@ -10,27 +10,47 @@ terraform {
 # Set the variable value in *.tfvars file
 
 provider "digitalocean" {
-  token = "DO002Q4MV2VAJ8EHPWMX/q0QY7Nec2B8"
+  token = "dop_v1_5920dc590621fd30519f8fe494eccc496e90e327a36b13e2eff1dcd72740e04f"
 }
 
-resource "digitalocean_droplet" "blue_server" {
-  name    = "blue-server"
-  region  = "nyc1"
-  size    = "s-1vcpu-1gb"
+# resource "digitalocean_droplet" "blue_server" {
+#   name    = "blue-server"
+#   region  = "nyc1"
+#   size    = "s-1vcpu-1gb"
+#   image   = "ubuntu-22-04-x64"
+#   ssh_keys = ["mykey"]
+
+#   user_data = <<-EOF
+#               #!/bin/bash
+#               apt-get update
+#               apt-get install -y software-properties-common
+#               apt-add-repository --yes --update ppa:ansible/ansible
+#               apt-get update
+#               apt-get install -y ansible
+
+#               # Configure firewall rules for ports 8701 to 8710
+#               for port in {8701..8710}; do
+#                 sudo ufw allow $port
+#               done
+#               EOF
+# }
+
+
+resource "digitalocean_droplet" "web" {
+  name = "test-web-vm"
   image   = "ubuntu-22-04-x64"
-  ssh_keys = ["mykey"]
+  region = "nyc1"
+  size = "s-1vcpu-1gb"
+  monitoring = true
+  ipv6 = true
+  private_networking = true
 
   user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y software-properties-common
-              apt-add-repository --yes --update ppa:ansible/ansible
-              apt-get update
-              apt-get install -y ansible
-
-              # Configure firewall rules for ports 8701 to 8710
-              for port in {8701..8710}; do
-                sudo ufw allow $port
-              done
-              EOF
+            #!/bin/bash
+            apt-get update
+            apt-get install -y software-properties-common
+            apt-add-repository --yes --update ppa:ansible/ansible
+            apt-get update
+            apt-get install -y ansible
+            EOF
 }
